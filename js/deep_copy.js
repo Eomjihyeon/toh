6a29178aa778606faf943eb032f33cdffca9fb48
hotan486 <hotan486@naver.com>
2018-10-06 16:30:06 +0900

    todo 수정버튼만들기2

---
+++ js/deep_copy.js
@@ -0,0 +1,5 @@
+let objA = { a : 1, b:2, c:3};
+let copyA = objA;
+copyA.a = 2;
+console.log(objA,copyA)
+
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,6 +10,8 @@ import {HeroService} from '../hero.service';
 export class TodoComponent implements OnInit {
   todoList: TodoVo[];
   newTodo = new TodoVo();
+  // 수정버튼 클릭시 todo_id 키 값에  TodoVO 객체를 저장
+  tempMap = new Map<number, TodoVo>();
 
   constructor(private heroService: HeroService) { }
 
@@ -34,14 +36,30 @@ export class TodoComponent implements OnInit {
         // inputbox 초기화
         this.newTodo.todo = null;
       });
-
   }
-  //수정 템플릿 변경
-  save(todo:TodoVo) {
+
+  /**
+   * 수정 템플릿으로 변경
+   */
+  save(todo: TodoVo) {
+    // 현재 값을 저장
+    // shallow copy는 같은 메모리 주소를 참고하기 때문에 안된다.
+    // this.tempMap.set(todo.todo_id, todo);
+    // 값을 복사하는 방법을 사용: deep copy
+    const tempTodo = {...todo};
+    console.log(tempTodo === todo);
+    this.tempMap.set(todo.todo_id, tempTodo);
+
     todo.isEdited = true;
   }
 
-  restore(item: TodoVo) {
-    item.isEdited = false;
+  restore(todo: TodoVo) {
+    // 기존값 복원
+    const tempTodo = this.tempMap.get(todo.todo_id);
+    // todo 객체에 tempTodo 객체 속성을 overwrite
+    // Object.assign(src, ...target);
+    Object.assign(todo, tempTodo);
+
+    todo.isEdited = false;
   }
 }
