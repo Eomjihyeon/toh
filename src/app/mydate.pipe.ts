d0c5e8b6c76a2db1bd9696d2a83bac14d2d15670
hotan486 <hotan486@naver.com>
2018-10-13 11:51:20 +0900

    pipe응용  date 날짜 데이트

--- src/app/mydate.pipe.ts
+++ src/app/mydate.pipe.ts
@@ -9,12 +9,13 @@ export class MydatePipe implements PipeTransform {
   // args:  파이프 뒤에 콜론 다음 스트링으로 받는다.
   // 리턴되는 값이 화면에 출력된다.
   transform(value: any, args?: any): any {
-    console.log('first:', value, 'second: ', args);
-    if (args === '$') {
-      return (value as number) / 1100 + args;
-    } else if (args === 'yen') {
-      return (value as number) / 120 + args;
-    }
+    // console.log('first:', value, 'second: ', args);
+    // if (args === '$') {
+    //   return (value as number) / 1100 + args;
+    // } else if (args === 'yen') {
+    //   return (value as number) / 120 + args;
+    // }
+    return (value as string).substring(0,16);
   }
 
 }
