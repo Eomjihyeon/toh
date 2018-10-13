ff922e3981533edda79ebb32b0b3ace68d786210
hotan486 <hotan486@naver.com>
2018-10-13 13:20:55 +0900

    바인딩(form양식)

--- src/app/admin/register-hero/register-hero.component.ts
+++ src/app/admin/register-hero/register-hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-register-hero',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-hero.component.scss']
 })
 export class RegisterHeroComponent implements OnInit {
+  form:FormGroup;
+//html에  formcontrol이름과 동일하게 해야함
+  constructor(private  fb:FormBuilder) {
+    this.form = this.fb.group({
+      name: null,
+      email: null,
+      sex: null,
+      country: null,
+      address: null,
+    });
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
